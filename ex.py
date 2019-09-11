c900fcacf33f07c0ba6ce1a93c2d36ce16477187
lefeb <philippe.lefebvre@ensicaen.fr>
2019-09-11 09:38:44 +0200

    ajout grid

--- ex.py
+++ ex.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(-5,5,100)
+print ('type(x) = ',type(x),' len = ', len(x) )
 plt.plot(x,np.sin(x))  # on utilise la fonction sinus de Numpy
 plt.ylabel('fonction sinus')
 plt.xlabel("l'axe des abcisses")
+plt.grid()
 plt.show()
